ed58462cc73ad9451ff9d6eabeb0d745cd99ebf4
Tanishq  dubey <tanishqdubey@macbook-pro.lan>
2020-08-07 16:12:20 -0700

    new branch

--- tanishq_dubey.py
+++ tanishq_dubey.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 
-Matrix_A = np.random.randn(2,3)
-print(Matrix_A)
+Matrix_D = np.random.randn(2,3)
+print(Matrix_D)
 
-Matrix_B = np.random.rand(3,4)
+Matrix_C = np.random.rand(3,4)
 
 
-final_result = np.matmul(Matrix_A, Matrix_B)
+final_result = np.matmul(Matrix_D, Matrix_C)
 print(final_result.shape)
+
+#lshjhajashjdjdhadijdhadhadjio
 
