1b20a2c1ca7cadfdc778b0d7450ff265e1ad0e03
Tanishq  dubey <tanishqdubey@macbook-pro.lan>
2020-08-07 15:22:55 -0700

    matrix multiplication

---
+++ tanishq_dubey.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+Matrix_A = np.random.randn(2,3)
+print(Matrix_A)
+
+Matrix_B = np.random.rand(3,4)
+
+
+final_result = np.matmul(Matrix_A, Matrix_B)
+print(final_result.shape)
+
